0c9079a48023bccd5c18f7936fb9e9da400e6ffe
Rafael Nakayama <RafaelNakayama2003@gmail.com>
2025-10-21 18:16:28 -0300

    Beta 1.0.9 - improvements to .csv formating, header still needs to be fixed

--- main.py
+++ main.py
@@ -42,13 +42,22 @@ def main_menu():
                 # Caminho para evitar erros em outros diretorios
                 caminho_csv = os.path.join(os.path.dirname(__file__), "data", "students.csv")
 
+                # Variavel Booleana que retorna True o csv ja foi criado e False se ainda nao
+                arquivo_existe = os.path.exists(caminho_csv)
+                
+                if arquivo_existe:
+                    vazio = os.path.getsize(caminho_csv)  # os.path.getsize ja pega o numero em bytes direto
+
                 # Registra no .csv as informacoes, coloquei no modo append para acrescentar e nunca sobrescrever
                 with open(caminho_csv, "a", newline='') as arquivocsv:
 
-                    chaves_csv = ["Nome", "Status", "Aulas", "Dia do Pagamento", "Nivel"]
+                    chaves_csv = ["Nome","Status","Aulas","Dia do Pagamento","Nivel"]
                     escritor = csv.DictWriter(arquivocsv, fieldnames=chaves_csv)
 
-                    escritor.writeheader()
+                    # Se o caminho nao existe ou existe mas esta vazio, escrevba o cabecalho
+                    if arquivo_existe == False or vazio == 0:
+                        escritor.writeheader()
+  
                     escritor.writerow({'Nome': f'{nome_do_aluno}', 
                                        'Status': f'{status_do_aluno}', 
                                        'Aulas': f'{aulas_assistidas}', 
@@ -58,7 +67,7 @@ def main_menu():
             elif option == 2:
                 # Abre e faz a leitura do .csv
                 with open(caminho_csv, newline='') as arquivocsv:
-                    leitor_csv = csv.reader(arquivocsv, delimiter=',', quotechar='|')
+                    leitor_csv = csv.DictReader(arquivocsv, delimiter=' ', quotechar='|')
                     for linha in leitor_csv:
                         print(', '.join(linha))
 
